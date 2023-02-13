fd004c39764f0d8e303ac75b638adbc7a320af7a
Sayemhaque <mdsayemofficial1000@gmail.com>
2023-02-13 01:20:17 -0800

    deposite input field valification added

--- deposite.js
+++ deposite.js
@@ -1,17 +1,41 @@
 // update balance
 
-//update deposite
-
 // deposite button
 const depositeBtn = document.getElementById("deposite-btn");
 //deposite input
 const depositeInputField = document.getElementById("deposite-input");
-//deposite amount 
+//deposite amount
 const depositeAmount = document.getElementById("deposite-amount");
+// error msg
+const depositeErrorMsg = document.getElementById("deposite-error");
+const WithDrawErrorMsg = document.getElementById("withdraw-error");
 
-// adding click event listener 
+const withdrawBtn = document.getElementById("withdraw-btn");
+//withdraw input
+const withdrawInputField = document.getElementById("withdraw-input");
+//withdraw amount
+const withdrawAmount = document.getElementById("withdraw-amount");
+//balance amount
+const totalBalance = document.getElementById("balance-amount");
 
-depositeBtn.addEventListener("click" , function () {
-    const depositeAmountValue = depositeInputField.value;
-  return  depositeAmount.innerText = Number(depositeAmountValue + depositeAmount);
-})
+// adding click event listener
+
+depositeBtn.addEventListener("click", function () {
+  const newDepositeAmount = depositeInputField.value;
+  const convertedDeopiteAmout = parseFloat(newDepositeAmount);
+  if(newDepositeAmount === ""){
+     depositeErrorMsg.innerText = "*Invalid Amount"
+  }
+ else{
+  depositeErrorMsg.innerText = ""
+  const updataedDiposite =
+    parseFloat(convertedDeopiteAmout) + parseFloat(depositeAmount.innerText);
+  depositeAmount.innerText = updataedDiposite;
+  const totalBalanceNumber = parseFloat(totalBalance.innerText)
+   totalBalance.innerText = totalBalanceNumber + updataedDiposite
+   depositeInputField.value = "";
+ }
+});
+
+
+
