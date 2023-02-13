2b7efa8946bf0f76a5240c37d57bfa26154096ec
Sayemhaque <mdsayemofficial1000@gmail.com>
2023-02-12 17:17:51 -0800

    form varification added

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // sumbit button
 const submitBtn = document.getElementById("submit-btn");
 
+//email-error msg
+const emialErrorMsg = document.getElementById("email-error");
+
+//password-error msg
+const passwordErrorMsg = document.getElementById("password-error");
+
 //adding click event listener
 
 submitBtn.addEventListener("click", function (e) {
@@ -17,10 +23,44 @@ submitBtn.addEventListener("click", function (e) {
   const password = passwordField.value;
 
   if (email === "sayem1000@gmail.com" && password === "joinmybank123") {
-    console.log("Log in successfull", { email, password });
-    emailField.value = ""
-    passwordField.value = ""
-  } else {
-    console.log("User not found");
+    window.location.href = "bank.html";
+    emailField.value = "";
+    passwordField.value = "";
   }
+   else if (email !== "sayem1000@gmail.com") {
+    if(email === ""){
+        emialErrorMsg.innerText = "*email required";
+        emailField.classList.add("error-border")
+        setTimeout(() => {
+          emialErrorMsg.innerText = "";
+          emailField.classList.remove("error-border")
+        }, 2000);
+    }else{
+        emialErrorMsg.innerText = "*Enter a valid email";
+        emailField.classList.add("error-border")
+        setTimeout(() => {
+          emialErrorMsg.innerText = "";
+          emailField.classList.remove("error-border")
+        }, 1000);
+    }
+   
+  } else if (password !== "joinmybank123") {
+    if(password === ""){
+        passwordErrorMsg.innerText = "*password required";
+        passwordField.classList.add("error-border")
+        setTimeout(() => {
+        passwordErrorMsg.innerText = "";
+        passwordField.classList.remove("error-border")
+        }, 1000);
+    }
+    else{
+        passwordErrorMsg.innerText = "*Enter a valid password";
+        passwordField.classList.add("error-border")
+        setTimeout(() => {
+          passwordErrorMsg.innerText = "";
+          passwordField.classList.remove("error-border")
+        }, 2000);
+   }
+  
+}
 });
